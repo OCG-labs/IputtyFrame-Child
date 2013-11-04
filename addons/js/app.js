8a82357513b9be05bb16290e220b76d3a9e16750
mrmatti3 <matt@iputty.com>
2013-11-04 10:09:00 -0800

    removed mouse over code from child
    added to master theme

--- addons/js/app.js
+++ addons/js/app.js
@@ -30,22 +30,3 @@ jQuery(window).load(function(){
 jQuery(function() {
     jQuery('.navbar li:has(ul)').doubleTapToGo();
 });
-
- /* JavaScript Media Queries */
-if (matchMedia) {
-    var mq = window.matchMedia("(min-width: 768px)");
-}
-
-//mouseover for menu     
-jQuery(function() {
-    jQuery('ul.nav li.dropdown').hover(function() {
-        if(mq.matches){
-            jQuery('.dropdown-menu', this).fadeIn();
-        }    
-    }, function() {
-        if(mq.matches) {
-            jQuery('.dropdown-menu', this).fadeOut('fast');
-        }
-    }); //hover
-}); //jQuery
-
