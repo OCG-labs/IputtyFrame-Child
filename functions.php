2d94ef914fe6f268809de0788281fbe559529660
Matthew Hansen <mattie02@gmail.com>
2013-11-04 11:13:15 -0800

    Update functions.php
    Remove Doubletap.js enqueue

--- functions.php
+++ functions.php
@@ -6,11 +6,9 @@ require_once('addons/widgets.php');
 
 if(!is_admin()){
      function load_user_js() {
-     	wp_register_script('doubleTap_js', get_stylesheet_directory_uri().'/addons/js/doubletaptogo.js', array("jquery"), '1.0.0', true);
      	wp_register_script('usrapp_js', get_stylesheet_directory_uri().'/addons/js/app.js', array("jquery"), '1.0.0', true);
      	wp_register_script('isotope_js', get_stylesheet_directory_uri().'/addons/js/jquery.isotope.js', array("jquery"), '1.5.25', true);
      	
-     	wp_enqueue_script('doubleTap_js');
      	wp_enqueue_script('usrapp_js');
      	wp_enqueue_script('isotope_js');
      }
