f1c5d9ad887ea17179ec373b13007b0db8f21895
Matthew Hansen <mattie02@gmail.com>
2013-12-10 13:08:51 -0800

    Update widgets.php

--- addons/widgets.php
+++ addons/widgets.php
@@ -1,17 +1,13 @@
 <?php
 
-function remove_some_widgets(){
-	unregister_sidebar( 'primary-widget-area' );
-}
-add_action( 'widgets_init', 'remove_some_widgets', 11 );
-
-
 if(function_exists('register_sidebar')) {
 	register_sidebar( array (
-		'name' => __('Homepage Event Widget 1', 'event-widget-1'), 
-		'id' => 'home-event-widget-1', 
-		'description' => __('Homepage event widget 1', 'dir'), 
-		'before_widget' => '<div id="home_event_1">', 
-		'after_widget' => '</div>'
+		'name' => __('Standard Sidebar', 'standard-sidebar'), 
+		'id' => 'standard-sidebar', 
+		'description' => __('Standard Sidebar', 'dir'), 
+		'before_widget' => '<div class="st_sidebar">', 
+		'after_widget' => '</div>', 
+		'before_title' => '<h3 class="st_sidebar_title">',
+		'after_title' => '</h3>'
 	) );
 }
